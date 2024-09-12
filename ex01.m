bdd671231258c6c850f930abcee1cf9c2836abf5
JeongSeoJin <pysuk88@gmail.com>
2024-09-12 11:38:08 +0900

    Add: ex01.m

---
+++ ex01.m
@@ -0,0 +1,7 @@
+a = sin(pi); %the value is not 0. but this is caused by computing
+b = 3*sin(pi); %this is also should be 0
+a+b
+
+y = 3+sin(pi) % this is 3
+
+%does not save files name as a number. file, variable name should have start with letter.
